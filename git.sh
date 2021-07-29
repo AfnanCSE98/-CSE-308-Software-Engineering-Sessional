f3e346010331f5a4050e9c58f6b7a1d7e75e2f2d
AfnanCSE98 <sihatafnan15.9.1997@gmail.com>
2021-07-30 03:48:30 +0600

    2021-07-30 03:48:30

--- git.sh
+++ git.sh
@@ -1,12 +1,25 @@
+#!/bin/bash
+
 git status
 
+echo 'enter commitMsg'
+read commitMsg
+
+# Use current date time in the commit message when its not passed
+if [[ -z "${message// }" ]]
+    then
+        message=$(date '+%Y-%m-%d %H:%M:%S')
+fi
+
+# stage all changes
 git add .
+echo "Staged all files"
 
-echo 'Enter the commit message:'
-read commitMessage
+# add commit
+git commit -m "$message"
+echo "Added the commit with message: '$message'"
 
-git commit -m "$commitMessage"
+git push -u origin main 
+echo "Push changes to main branch"
 
-git push -u origin main
 
-read
